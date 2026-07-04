e43944bd5ce4eab189477c4441c0a05f2d1bfc64
AlbertoS82 <albertonkio82@gmail.com>
2026-07-04 12:09:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'myallenamento-v1';
+const CACHE = 'myallenamento-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e=>{
